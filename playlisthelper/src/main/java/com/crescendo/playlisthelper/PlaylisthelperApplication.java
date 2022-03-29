0e9d6af14cf2f2a6c72ed6f75fcebe1adeec2c62
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-03-29 12:22:36 +0100

    first commit

---
+++ playlisthelper/src/main/java/com/crescendo/playlisthelper/PlaylisthelperApplication.java
@@ -0,0 +1,13 @@
+package com.crescendo.playlisthelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylisthelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylisthelperApplication.class, args);
+	}
+
+}
---
+++ playlisthelper/src/test/java/com/crescendo/playlisthelper/PlaylisthelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crescendo.playlisthelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylisthelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
