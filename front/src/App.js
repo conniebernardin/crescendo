4981328254f141c61a886e8d8b43741ff355748a
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-03-30 16:08:45 +0100

    added song states to App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,29 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PlaylistPage from './Pages/PlaylistPage';
 import HomePage from './Pages/HomePage';
 
 function App() {
+    
+    const [songs, setSongs] = useState([]);
+    const [loading, setLoading] = useState(true);
 
 
-
-useEffect(()=>{ fetch("http://localhost:8080/joins/alljoin")
-    .then(response => response.json())
-    .then(response => console.log(response))
-},[])
+    useEffect(()=>{ 
+        fetch("http://localhost:8080/joins/alljoin")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => setSongs(data))
+        .then(console.log(songs))
+        .then(setLoading(false))
+    },[])
 
  
   return(
   <>
   <h1>hiyaaaa</h1>
+  {loading ? <h2>Not loaded</h2> : <h2>Has loaded</h2>}
   <Router>
       <Routes>
           <Route exact path= "/" element={<HomePage />} />
--- playlisthelper/src/main/java/com/crescendo/playlisthelper/Configurations/SpringGlobalConfig.java
+++ playlisthelper/src/main/java/com/crescendo/playlisthelper/Configurations/SpringGlobalConfig.java
@@ -1,2 +1,26 @@
-package com.crescendo.playlisthelper.Configurations;public class SpringGlobalConfig {
+package com.crescendo.playlisthelper.Configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+// SpringGlobalConfig.java
+
+@Configuration
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Configure which route in the controller the mapping applies to
+        registry.addMapping("/**")
+                // Accept requests originating from this location
+                .allowedOrigins("*")
+                // Acceptable parameters for headers, eg. authentication, content-type, etc
+                // We leave the wildcard here as there are many more headers to consider than we've covered so far
+                .allowedHeaders("*")
+                // HTTP methods the origin is allowed to make
+                .allowedMethods("*");
+    }
+
 }
