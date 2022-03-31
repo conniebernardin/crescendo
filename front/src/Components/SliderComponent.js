e46c8956d221aa9ee038c06a4cb1d4ddaf6081c2
Rosalinda Tome <rosalinda_tome@hotmail.com>
2022-03-31 14:56:30 +0100

    sliderValue function added but not completed

--- front/src/Components/SliderComponent.js
+++ front/src/Components/SliderComponent.js
@@ -3,16 +3,36 @@ import ReactSlider from "react-slider";
 
 const SliderComponent = () => {
     
+    const [sliderValue, setSliderValue] = useState (5)
+    const onSlide = (event) => {
+        // const newValue = event.target.mark 
+        // setSliderValue(parseInt(newValue))
+
+        // this.setSliderValue({value : event.target.value})
+
+        console.log("hello" + event.target);
+
+    }
+
+
+
     return(
+
         <ReactSlider
-        className="horizontal-slider"
-        thumbClassName="example-thumb"
-        trackClassName="example-track"
+            onChange={onSlide}
+            className="horizontal-slider"
+            marks
+            markClassName="example-mark"
+            min={1}
+            max={10}
+            thumbClassName="example-thumb"
+            trackClassName="example-track"
+            renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
         />
-        
-     
     )
 
 }
 
 export default SliderComponent
+
+
