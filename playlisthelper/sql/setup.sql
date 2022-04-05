35ddcd966c66185985e7645a5542fe6e75e61f63
Rosalinda Tome <rosalinda_tome@hotmail.com>
2022-04-05 15:22:27 +0100

    clearing up code in sql

--- playlisthelper/sql/setup.sql
+++ playlisthelper/sql/setup.sql
@@ -210,7 +210,6 @@ INSERT INTO artists(artist_name)
 INSERT INTO artists(artist_name)
  VALUES ('Lay Zhang');
 
-//end at 32
 
 
 
@@ -334,3 +333,251 @@ INSERT INTO joins (artist_id, song_id) VALUES (31,48);
 INSERT INTO joins (artist_id, song_id) VALUES (31,49);
 INSERT INTO joins (artist_id, song_id) VALUES (31,50);
 INSERT INTO joins (artist_id, song_id) VALUES (32,51);
+
+
+
+INSERT INTO artists(artist_name)
+VALUES ('Ms. Lauryn Hill');
+
+INSERT INTO artists(artist_name)
+VALUES ('India Arie');
+
+INSERT INTO artists(artist_name)
+VALUES ('Amy Winehouse');
+
+INSERT INTO artists(artist_name)
+VALUES ('NAO');
+
+INSERT INTO artists(artist_name)
+VALUES ('Jazmine Sullivan');
+
+INSERT INTO artists(artist_name)
+VALUES ('Frank Ocean');
+
+INSERT INTO artists(artist_name)
+VALUES ('Giveon');
+
+INSERT INTO artists(artist_name)
+VALUES ('Mahalia');
+
+INSERT INTO artists(artist_name)
+VALUES ('Summer Walker');
+
+INSERT INTO artists(artist_name)
+VALUES ('Alicia Keys');
+
+INSERT INTO artists(artist_name)
+VALUES ('Erykah Badu');
+
+INSERT INTO artists(artist_name)
+VALUES ('Macy Gray');
+
+INSERT INTO artists(artist_name)
+VALUES ('Tracy Chapman');
+
+INSERT INTO artists(artist_name)
+VALUES ('Adele');
+
+INSERT INTO artists(artist_name)
+VALUES ('Daniel Merriweather');
+
+INSERT INTO artists(artist_name)
+VALUES ('Jhene Aiko');
+
+INSERT INTO artists(artist_name)
+VALUES ('Lianne La Havas');
+
+INSERT INTO artists(artist_name)
+VALUES ('Snoh Aalegra');
+
+INSERT INTO artists(artist_name)
+VALUES ('dvsn');
+
+INSERT INTO artists(artist_name)
+VALUES ('Ella Mai');
+
+
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('To Zion', 4, 43, 'neosoul', 'english', 'https://www.youtube.com/embed/1sQjh261rU8', 370);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Killing Me Softly With His Song', 3, 45, 'neosoul', 'english', 'https://www.youtube.com/embed/H-RBJNqdnoM', 298);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Get It Together', 2, 47, 'neosoul', 'english', 'https://www.youtube.com/embed/V7LwmMTD3B4', 294);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Brown Skin', 5, 60, 'neosoul', 'english', 'https://www.youtube.com/embed/12kMTnObZcM', 218);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Back to Black', 1, 31, 'neosoul', 'english', 'https://www.youtube.com/embed/TJAfLE39ZZ8', 240);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Love Is a Losing Game', 2, 20, 'neosoul', 'english', 'https://www.youtube.com/embed/nMO5Ko_77Hk',  168);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Drive and Disconnect', 7, 115, 'neosoul', 'english', 'https://www.youtube.com/embed/FWK5pTukS9s', 210);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('If You Ever' , 7, 120, 'neosoul', 'english', 'https://www.youtube.com/embed/7bRQmZXH6PE', 222);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Bust Your Windows', 1, 54, 'neosoul', 'english', 'https://www.youtube.com/embed/mOzdfaEPaR0', 261);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Dream Big', 9, 110, 'neosoul', 'english', 'https://www.youtube.com/embed/K5w85EWZqKE', 197);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Pink + White', 5, 27, 'neosoul', 'english', 'https://www.youtube.com/embed/uzS3WG6__G4', 184);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Lost', 3, 31, 'neosoul', 'english', 'https://www.youtube.com/embed/J3DWAJGaf7o', 232);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Heartbreak Anniversary', 2, 33, 'neosoul', 'english', 'https://www.youtube.com/embed/nja_0BaQcNg', 198);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Vanish', 2, 44, 'neosoul', 'english', 'https://www.youtube.com/embed/JTCGZCPt7Is', 210);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Sober Jarreau Vandal Remix', 4, 51, 'neosoul', 'english', 'https://www.youtube.com/embed/cRLOgf7ypBY', 220);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('What You Did', 4, 30, 'neosoul', 'english', 'https://www.youtube.com/embed/azA0RJMUK0E', 262);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Girls Need Love', 2, 24, 'neosoul', 'english', 'https://www.youtube.com/embed/1l8vIAvAerE', 153);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Playing Games', 3, 29, 'neosoul', 'english', 'https://www.youtube.com/embed/SDSXLkD74KE', 147);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('No One', 2, 96, 'neosoul', 'english', 'https://www.youtube.com/embed/rywUS-ohqeE', 248);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Empire State of Mind Part Two', 7, 24, 'neosoul', 'english', 'https://www.youtube.com/embed/_W44NZvHc9I', 216);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Love of My Life', 8, 44, 'neosoul', 'english', 'https://www.youtube.com/embed/dNk3R23Twgw', 268);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('On & On', 5, 40, 'neosoul', 'english', 'https://www.youtube.com/embed/TW28iWV7nxE', 226);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('I Try', 3, 38, 'neosoul', 'english', 'https://www.youtube.com/embed/WEQ0l_m3Xm0', 236);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('First Time', 3, 26, 'neosoul', 'english', 'https://www.youtube.com/embed/voO_ZHqq6oY', 201);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Fast Car', 5, 26, 'neosoul', 'english', 'https://www.youtube.com/embed/DwrHwZyFN7M', 296);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Baby Can I hold You', 1, 37, 'neosoul', 'english', 'https://www.youtube.com/watch?v=DqoLMGSBGYc', 194);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Easy On Me', 1, 71, 'neosoul', 'english', 'https://www.youtube.com/embed/X-yIEMduRXk', 225);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Set Fire to the Rain', 1, 110, 'neosoul', 'english', 'https://www.youtube.com/embed/a2giXO6eyuI', 243);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Red', 2, 48, 'neosoul', 'english', 'https://www.youtube.com/embed/o1NCmDYeymo', 252);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Change', 4, 49, 'neosoul', 'english', 'https://www.youtube.com/embed/gvz2PLiHyo4', 202);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Oblivion', 2, 25, 'neosoul', 'english', 'https://www.youtube.com/embed/9gi4lmcgI4k', 350);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('The Worst', 2, 46, 'neosoul', 'english', 'https://www.youtube.com/embed/oEm3lY3trRU', 278);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Bittersweet', 3, 35, 'neosoul', 'english', 'https://www.youtube.com/embed/W6Ak2_TJ5zs', 292);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Green & Gold', 8, 48, 'neosoul', 'english', 'https://www.youtube.com/embed/qnogG7IMj8o', 213);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('I Want You Around', 8, 42, 'neosoul', 'english', 'https://www.youtube.com/embed/4976Fgvf5Ps', 234);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Whoa', 9, 48, 'neosoul', 'english', 'https://www.youtube.com/embed/rzRh6Eon5DQ', 200);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('A Muse', 10, 67, 'neosoul', 'english', 'https://www.youtube.com/embed/R98nW-TfTIk',239);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Between Us', 9, 64, 'neosoul', 'english', 'https://www.youtube.com/embed/YsLCxGP7woA', 206);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Boo"d up', 10, 41, 'neosoul', 'english', 'https://www.youtube.com/embed/6YNZlXfW6Ho', 241);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+VALUES ('Trip', 8, 44, 'neosoul', 'english', 'https://www.youtube.com/embed/JfGD75vHWrU', 218);
+
+-- joining artist ID and song ID:
+
+INSERT INTO joins (artist_id, song_id) VALUES (33, 52);
+INSERT INTO joins (artist_id, song_id) VALUES (33, 53);
+
+INSERT INTO joins (artist_id, song_id) VALUES (34, 54);
+INSERT INTO joins (artist_id, song_id) VALUES (34, 55);
+
+INSERT INTO joins (artist_id, song_id) VALUES (35, 56);
+INSERT INTO joins (artist_id, song_id) VALUES (35, 57);
+
+INSERT INTO joins (artist_id, song_id) VALUES (36, 58);
+INSERT INTO joins (artist_id, song_id) VALUES (36, 59);
+
+INSERT INTO joins (artist_id, song_id) VALUES (37, 60);
+INSERT INTO joins (artist_id, song_id) VALUES (37, 61);
+
+INSERT INTO joins (artist_id, song_id) VALUES (38, 62);
+INSERT INTO joins (artist_id, song_id) VALUES (38, 63);
+
+INSERT INTO joins (artist_id, song_id) VALUES (39, 64);
+INSERT INTO joins (artist_id, song_id) VALUES (39, 65);
+
+INSERT INTO joins (artist_id, song_id) VALUES (40, 66);
+INSERT INTO joins (artist_id, song_id) VALUES (40, 67);
+
+INSERT INTO joins (artist_id, song_id) VALUES (41, 68);
+INSERT INTO joins (artist_id, song_id) VALUES (41, 69);
+
+INSERT INTO joins (artist_id, song_id) VALUES (42, 70);
+INSERT INTO joins (artist_id, song_id) VALUES (42, 71);
+
+INSERT INTO joins (artist_id, song_id) VALUES (43, 72);
+INSERT INTO joins (artist_id, song_id) VALUES (43, 73);
+
+INSERT INTO joins (artist_id, song_id) VALUES (44, 74);
+INSERT INTO joins (artist_id, song_id) VALUES (44, 75);
+
+INSERT INTO joins (artist_id, song_id) VALUES (45, 76);
+INSERT INTO joins (artist_id, song_id) VALUES (45, 77);
+
+INSERT INTO joins (artist_id, song_id) VALUES (46, 78);
+INSERT INTO joins (artist_id, song_id) VALUES (46, 79);
+
+INSERT INTO joins (artist_id, song_id) VALUES (47, 80);
+INSERT INTO joins (artist_id, song_id) VALUES (47, 81);
+
+INSERT INTO joins (artist_id, song_id) VALUES (48, 82);
+INSERT INTO joins (artist_id, song_id) VALUES (48, 83);
+
+INSERT INTO joins (artist_id, song_id) VALUES (49, 84);
+INSERT INTO joins (artist_id, song_id) VALUES (49, 85);
+
+INSERT INTO joins (artist_id, song_id) VALUES (50, 86);
+INSERT INTO joins (artist_id, song_id) VALUES (50, 87);
+
+INSERT INTO joins (artist_id, song_id) VALUES (51, 88);
+INSERT INTO joins (artist_id, song_id) VALUES (51, 89);
+
+INSERT INTO joins (artist_id, song_id) VALUES (52, 90);
+INSERT INTO joins (artist_id, song_id) VALUES (52, 91);
+
+
