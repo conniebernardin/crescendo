5546c93352b1c468b764349abc6536ddab85ff6e
Sarina Salamon <salamon.sarina@gmail.com>
2022-04-05 11:48:01 +0100

    sarina sql

---
+++ playlisthelper/sql/sarina.sql
@@ -0,0 +1,187 @@
+INSERT INTO artists(artist_name)
+ VALUES ('Tame Impala');
+
+ INSERT INTO artists(artist_name)
+ VALUES ('Phoebe Bridgers');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Gus Dapperton');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Wet Leg');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Peach Pit');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Foals');
+
+INSERT INTO artists(artist_name)
+ VALUES ('The Internet');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Glass Animals');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Hozier');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Arctic Monkeys');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Rina Sawayama');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Florence + The Machine');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Lorde');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Rex Orange County');
+
+INSERT INTO artists(artist_name)
+ VALUES ('Mitski');
+
+ INSERT INTO artists(artist_name)
+ VALUES ('Jungle');
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Let It Happen', 5, 125, 'indie', 'english', 'https://www.youtube.com/watch?v=pFptt7Cargc&ab_channel=tameimpalaVEVO', 256);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('The Less I Know The Better', 6, 117, 'indie', 'english', 'https://www.youtube.com/watch?v=2SUwOgmvzK4&ab_channel=tameimpalaVEVO', 217);
+
+
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('I Know The End', 2, 111, 'indie', 'english','https://www.youtube.com/watch?v=WJ9-xN6dCW4&ab_channel=PhoebeBridgers', 346);
+
+
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+  VALUES ('Post Humorous', 4, 120, 'indie', 'english', 'https://www.youtube.com/watch?v=G8G3Tcc0sns&ab_channel=GusDapperton', 303);
+
+
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Chaise Longue', 7, 160, 'indie', 'english','https://www.youtube.com/watch?v=Zd9jeJk2UHQ&ab_channel=WetLegVEVO', 216);
+
+
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+  VALUES ('Alrighty Aphrodite', 6, 82, 'indie', 'english', 'https://www.youtube.com/watch?v=ICNtZKmQGMw&ab_channel=PEACHPIT', 217);
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link length)
+  VALUES ('Being so Normal', 7, 148, 'indie', 'english', 'https://www.youtube.com/watch?v=3SWXKmi30XY&ab_channel=PEACHPIT', 220);
+
+
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Spanish Sahara', 2, 128, 'indie', 'english', 'https://www.youtube.com/watch?v=eYoINidnLRQ&ab_channel=Foals', 413);
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('London Thunder', 3, 92, 'indie', 'english', 'https://www.youtube.com/watch?v=UAiYLEsJNpY&ab_channel=Foals-Topic', 254);
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Mountain at My Gates', 4, 104, 'indie', 'english', 'https://www.youtube.com/watch?v=l_EIE5f2t6M&ab_channel=Foals', 251);
+
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+  VALUES ('Special Affair', 6, 120, 'indie', 'english', 'https://www.youtube.com/watch?v=CNJkas_LiCM&ab_channel=TheInternetVEVO', 291);
+
+   INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+  VALUES ('Penthouse Cloud', 3, 89, 'indie', 'english', 'https://www.youtube.com/watch?v=Kg6iQCCBTlE&ab_channel=TheInternet-Topic', 284);
+
+   INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+  VALUES ('La Di Da', 8, 150, 'indie', 'english', 'https://www.youtube.com/watch?v=-xyDg9Tco5s&ab_channel=TheInternetVEVO', 296);
+
+
+     INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Heat Waves', 6, 81, 'indie', 'english', 'https://www.youtube.com/watch?v=mRD0-GxqHVo&ab_channel=GlassAnimalsVEVO', 235);
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('The Other Side Of Paradise', 9, 128, 'indie', 'english', 'https://www.youtube.com/watch?v=RMPX_vgqQnM&ab_channel=GlassAnimals-Topic', 320);
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Agnes', 3, 165, 'indie', 'english', 'https://www.youtube.com/watch?v=PhdtdUljThU&ab_channel=GlassAnimalsVEVO', 234);
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Tokyo Drifting', 9, 129, 'indie', 'english', 'https://www.youtube.com/watch?v=R_7HqeukRrI&ab_channel=GlassAnimalsVEVO', 240);
+
+
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Cherry Wine', 4, 82, 'indie', 'english', 'https://www.youtube.com/watch?v=SdSCCwtNEjA&ab_channel=HozierVEVO', 253);
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Movement', 4, 144, 'indie', 'english', 'https://www.youtube.com/watch?v=OSye8OO5TkM&ab_channel=HozierVEVO', 235);
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Take Me to Church', 7, 129, 'indie', 'english', 'https://www.youtube.com/watch?v=PVjiKRfKpPI&ab_channel=HozierVEVO', 256);
+
+
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Crying Lightning', 8, 107, 'indie', 'english', 'https://www.youtube.com/watch?v=fLsBJPlGIDU&ab_channel=DominoRecordingCo.', 223);
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('505', 3, 140, 'indie', 'english', 'https://www.youtube.com/watch?v=qU9mHegkTc4&ab_channel=ArcticMonkeys-Topic', 253);
+
+    INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+   VALUES ('Fluorescent Adolescent', 6, 112, 'indie', 'english', 'https://www.youtube.com/watch?v=ma9I9VBKPiw&ab_channel=DominoRecordingCo.', 195);
+
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('XS', 9, 117, 'indie', 'english', 'https://www.youtube.com/watch?v=TO2c06p6m5w&ab_channel=RinaSawayamaVEVO', 218);
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Comme Des Garçons (Like The Boys)', 8, 119, 'indie', 'english', 'https://www.youtube.com/watch?v=uv5i5yg-zNc&ab_channel=RinaSawayamaVEVO', 183);
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Cherry', 3, 102, 'indie', 'english', 'https://www.youtube.com/watch?v=wn5YFnrD1u8&ab_channel=RinaSawayamaVEVO', 209);
+
+
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Dog Days Are Over', 7, 150, 'indie', 'english', 'https://www.youtube.com/watch?v=iWOyfLBYtuU&ab_channel=FlorenceMachineVEVO', 223);
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Delilah', 8, 170, 'indie', 'english', 'https://www.youtube.com/watch?v=gJ50rvySDCk&ab_channel=FlorenceMachineVEVO', 294);
+
+
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Glory And Gore', 4, 144, 'indie', 'english', 'https://www.youtube.com/watch?v=UyF3h5AKGEk&ab_channel=Lorde', 210);
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Tennis Court', 8, 90, 'indie', 'english', 'https://www.youtube.com/watch?v=D8Ymd-OCucs&ab_channel=LordeVEVO', 201);
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Mood Ring', 3, 95, 'indie', 'english', 'https://www.youtube.com/watch?v=P103bWMdvtA&ab_channel=LordeVEVO', 224);
+
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Television / So Far So Good', 7, 165, 'indie', 'english', 'https://www.youtube.com/watch?v=NZc__Hhi4L8&ab_channel=RexOrangeCounty', 262);
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('10/10', 7, 188, 'indie', 'english', 'https://www.youtube.com/watch?v=TozTL-MJCB8&ab_channel=RexOrangeCounty', 172);
+
+
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Nobody', 2, 117, 'indie', 'english', 'https://www.youtube.com/watch?v=qooWnw5rEcI&ab_channel=MitskiVEVO', 323);
+
+  INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('First Love / Late Spring', 4, 100, 'indie', 'english', 'https://www.youtube.com/watch?v=WCphVz0ZGns&ab_channel=Mitski-Topic', 278);
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Working for the Knife', 3, 83, 'indie', 'english', 'https://www.youtube.com/watch?v=t6hN8nGcpY8&ab_channel=mitskimitski', 159);
+
+
+
+INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('Truth', 7, 155, 'indie', 'english', 'https://www.youtube.com/watch?v=TcP8zQPQOEM&ab_channel=Jungle4eva', 252);
+
+ INSERT INTO songs (song_title, mood, tempo, genre, language, link, length)
+ VALUES ('House in LA', 3, 117, 'indie', 'english', 'https://www.youtube.com/watch?v=J6dtXL_P2b8&ab_channel=JungleVEVO', 292);
